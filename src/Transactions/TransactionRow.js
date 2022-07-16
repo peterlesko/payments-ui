1e1279e401d7e318b8ab113d4c5ff396e7c3abe7
Peter Lesko <peterlesko@yahoo.com>
2022-07-16 11:24:01 +0000

    Challenge completed rec 4.7.22 4:54:00, populate table

--- src/Transactions/TransactionRow.js
+++ src/Transactions/TransactionRow.js
@@ -1,7 +1,22 @@
 const TransactionRow = (props) => {
 
-  return <tr><td>{props.id}</td><td>{props.date}</td><td>{props.country}</td>
-  <td>{props.currency}</td><td>{props.amount}</td></tr>;
+  return <tr>
+      <td>{props.transactionRow.id}</td>
+      <td>{props.transactionRow.date}</td>
+      <td>{props.transactionRow.country}</td>
+      <td>{props.transactionRow.currency}</td>
+      <td>{props.transactionRow.amount}</td>
+    </tr>;
+
+  //before challange-populating table 
+  // return <tr>
+  //     <td>{props.id}</td>
+  //     <td>{props.date}</td>
+  //     <td>{props.country}</td>
+  //     <td>{props.currency}</td>
+  //     <td>{props.amount}</td>
+  //   </tr>;
+
 }
 
 export default TransactionRow;
--- src/Transactions/Transactions.js
+++ src/Transactions/Transactions.js
@@ -1,15 +1,33 @@
 import './Transactions.css'
 import TransactionRow from './TransactionRow';
+import { getAllPayments } from '../data/DataFunctions';
+import { useState } from 'react';
 
 const Transactions = () => {
 
+  const allPayments = getAllPayments();
+  const displayPayments = allPayments.map((it, index) => <TransactionRow key={index} transactionRow={it} />); 
+
+  //alternative solution
+  // const allPayments = getAllPayments;
+  // const [eachPayment, setPayments] = useState(allPayments);
+  // const displayPayments = eachPayment.map((it, index) => <TransactionRow key={index} transactionRow={it} />); 
+
   return <table id="transactionTable" style={{background: "#ccc"}} className="transactionsTable">
     <thead>
-      <tr><th>Id</th><th>Date</th><th>Country</th><th>Currency</th><th>Amount</th></tr>
+      <tr>
+        <th>Id</th>
+        <th>Date</th>
+        <th>Country</th>
+        <th>Currency</th>
+        <th>Amount</th>
+      </tr>
     </thead>
     <tbody>
-      <TransactionRow id="1" date="2022-07-02" country="USA" currency="USD" amount="102" />
-      <TransactionRow id="2" date="2022-07-04" country="Ireland" currency="EUR" amount="67" />
+      {/* <TransactionRow id="1" date="2022-07-02" country="USA" currency="USD" amount="102" />
+      <TransactionRow id="2" date="2022-07-04" country="Ireland" currency="EUR" amount="67" /> */}
+        {displayPayments}
+
       {/* <tr><td>1</td><td>20222-07-04</td><td>USA</td><td>USD</td><td>102</td></tr>
       <tr><td>2</td><td>20222-07-04</td><td>Ireland</td><td>EUR</td><td>67</td></tr> */}
     </tbody>
